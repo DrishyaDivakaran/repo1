c555715437a9c557bee4be426e47b433866c6a03
DrishyaDivakaran <drishyadivakaran000@gmail.com>
2022-02-01 22:18:16 +0530

    user1 first committ

---
+++ src/test/java/DemoGit/Pratice1.java
@@ -0,0 +1,12 @@
+package DemoGit;
+
+import org.testng.annotations.Test;
+
+public class Pratice1 {
+	@Test
+	public void demoGit()
+	{
+		System.out.println("this is user 1 first commit");
+	}
+
+}
